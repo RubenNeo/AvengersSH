23dee25245acfecb0039fc21ed87e1b42d849956
RubenNeo <ruben.lazaronp@gmail.com>
2024-06-28 09:39:58 +0200

    ya he aplicado la escala del DetailsSuperHeroActivity.

--- app/src/main/java/com/example/avengerssh/activities/DetailsSuperHeroActivity.kt
+++ app/src/main/java/com/example/avengerssh/activities/DetailsSuperHeroActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.animation.Animation
+import android.view.animation.ScaleAnimation
 import android.widget.ImageView
 import android.widget.Toast
 import com.example.avengerssh.R
@@ -37,11 +39,22 @@ class DetailsSuperHeroActivity : AppCompatActivity() {
 
 
         val id = intent.getIntExtra("SUPERHERO_ID", -1)
-
-
         GetById(id)
+        //para que al clickar en el boton del heroe me salga la animacion a escala del ImageView
+        val scaleAnimation = ScaleAnimation(
+            0f, 1f, // Escala de X: de 0% a 100%
+            0f, 1f, // Escala de Y: de 0% a 100%
+            Animation.RELATIVE_TO_SELF, 0.5f, // Pivot X: en el centro de la vista
+            Animation.RELATIVE_TO_SELF, 0.5f  // Pivot Y: en el centro de la vista
+        ).apply {
+            duration = 2000 // Duración de la animación en milisegundos
+            fillAfter = true // Mantener el estado final después de la animación
+        }
 
+        // Iniciar la animación
+        binding.ivSuperhero.startAnimation(scaleAnimation)
     }
+
 
     private fun loadData() {
         binding.tvSuperHeroname.text = superhero.name
